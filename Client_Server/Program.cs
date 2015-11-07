ec71f5e35a1a2524244ea3e4c7cd463c322d39f4
MohammedAbuissa <mohammed.abuissa@outlook.com>
2015-11-07 19:36:19 +0200

    Random input generator and a better but bad look

--- Client_Server/Program.cs
+++ Client_Server/Program.cs
@@ -11,42 +11,49 @@ namespace Client_Server
         static void Main(string[] args)
         {
             bool connection;
-            Console.WriteLine("Type of Connection enter T to TCP or U to UDP");
+     l:     Console.WriteLine("Type of Connection enter T to TCP or U to UDP");
             String C = Console.ReadLine();
             if(C.Contains("T"))
             {
                 connection = true;
             }
-            else
+            else if(C.Contains("U"))
             {
                 connection = false;
             }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+                goto l;
+            }
             Console.WriteLine("Enter Port number for the server");
             int port = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter S to Server and C to Client");
+     t:     Console.WriteLine("Enter S to Server and C to Client");
             C = Console.ReadLine();
             if (C.Contains("C"))
             {
                 Console.WriteLine("Enter the Count of Clients");
                 int Count = int.Parse(Console.ReadLine());
                 int[] Numbers = new int[Count * 2];
-                Console.WriteLine("Enter Count of Clients Lines containing 2 space-seperate integers");
-                for (int i = 0; i < Count*2; i+=2)
+                Console.WriteLine("Press enter to start generating numbers");
+                Console.ReadLine();
+                Random InputGenerator = new Random(DateTime.Now.Millisecond);
+                for (int i = 0; i < Count * 2; i += 2)
                 {
-                    string[] S = Console.ReadLine().Split(new char[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
-                    if (S.Length < 2)
-                        for (int j = S.Length; j < 2; j++)
-                            S[j] = "0";
                     for (int j = 0; j < 2; j++)
                     {
-                        Numbers[i + j] = int.Parse(S[j]);
+                        Numbers[i + j] = InputGenerator.Next()%33554432;
+                        Console.Write(Numbers[i+j].ToString() + " ");
                     }
+                    Console.WriteLine();
                 }
                 Client Client;
                 if (connection)
                     Client = new TCPClient(port, new System.Net.IPAddress(new byte[] { 127, 0, 0, 1 }));
                 else
                     Client = new UDPCLient(port, new System.Net.IPAddress(new byte[] { 127, 0, 0, 1 }));
+                Console.WriteLine("Press enter to start simulation");
+                Console.ReadLine();
                 Client.Start(Numbers);
             }
             else if (C.Contains("S"))
@@ -58,6 +65,12 @@ namespace Client_Server
                     s = new UDPServer(port, new System.Net.IPAddress(new Byte[] { 127, 0, 0, 1 }));
                 s.Start();
             }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+                goto t;
+            }
+
         }
     }
 }
